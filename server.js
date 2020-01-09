0ad1519610846c0c8e062f9e90d7e7b7281b1c0c
mdjack sparrow <mdjack01122000@gmail.com>
2020-01-10 00:16:41 +0530

    commit changes

--- server.js
+++ server.js
@@ -1,2 +1,17 @@
 // Requirement modules
-const express = require("")
+const express = require('express');
+const bodyParser = require('body-parser');
+const request = require('request');
+const mongoose = require('mongodb');
+
+// import express function 
+const app = express();
+
+// use modules
+app.use(bodyParser.urlencoded({extended: true}));
+// app.use(express.static('public'));
+
+// Create server listen port: 4000
+app.listen(progress.env.PORT || 4000, () => {
+ console.log("Server running on port : 4000");
+})
